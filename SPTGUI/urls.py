4db96dd9f24cdeffbae98115648c820aab1fcba4
maxime <maxime@alice>
2017-01-27 17:35:07 +0100

    Upload canvas
    The upload is not working yet, but the Flow.js code is here for that.

--- SPTGUI/urls.py
+++ SPTGUI/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name =  'SPTGUI'
 
 urlpatterns = [
+    url(r'^upload/$', views.upload_tmp, name='upload_tmp'),    ## TEMPORARY
     url(r'^analysis/$', views.analysis_root, name='analysis_root'),    
     url(r'^analysis/(?P<url_basename>.+)/$', views.analysis, name='analysis'),
     url(r'^$', views.index, name='index'),
--- SPTGUI/views.py
+++ SPTGUI/views.py
@@ -8,18 +8,27 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
+    """Main view"""
     template = loader.get_template('SPTGUI/homepage.html')
     context = {'url_basename': get_unused_namepage()}
     return HttpResponse(template.render(context, request))
 
+## ==== DEBUG
+def upload_tmp(request):
+    template = loader.get_template('SPTGUI/upload_tmp.html')
+    return HttpResponse(template.render(request))
+
+## ==== Views
 def analysis_root(request):
     return HttpResponse("There's nothing here...")
 
 def analysis(request, url_basename):
+    """Returns the analysis view"""
     template = loader.get_template('SPTGUI/analysis.html')
     context = {'url_basename': url_basename}
     return HttpResponse(template.render(context, request))
 
+## ==== Auxiliary functions
 def get_unused_namepage():
     """Function returns an unused, 10 chars identifier for an analysis"""
     N=10
