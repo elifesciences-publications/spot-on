5c88f4b08e1fafe228491af5aea0860e2e76f201
maxime <maxime@alice>
2017-01-30 18:48:46 +0100

    Upload: list existing datasets

--- SPTGUI/static/SPTGUI/analysis.js
+++ SPTGUI/static/SPTGUI/analysis.js
@@ -2,24 +2,56 @@
 
     // From https://thinkster.io/angular-tabs-directive
     angular.module('app', ['flow'])
-	.controller('UploadController', ['$scope', function($scope) {
+    
+	.service('getterService', function($http) {
+	    // This service handles $http requests to get the list fo the datasets
+	    this.getDatasets = function(callback) {
+		return $http.get('./api/datasets');
+	    }
+	})
+
+    
+	.controller('UploadController', ['$scope', 'getterService', function($scope, getterService) {
+	    // This is the controller for the upload part of the app
+
+	    //
+	    // ==== Let's first define some global variables (on the $scope).
+	    //
 	    $scope.currentlyUploading=false;
 	    $scope.successfullyUploaded=0;
+	    getterService.getDatasets().then(function(dataResponse) {
+		$scope.datasets = dataResponse['data'];
+		$scope.successfullyUploaded=dataResponse['data'].length;
+	    });// Populate the scope with the already uploaded datasets
 	    
-	    // This event bound when a file is added
+	    //
+	    // ==== Upload (ng-flow) events
+	    //
+	    // (1) Fired when a file is added to the download list
 	    $scope.$on('flow::fileAdded', function (event, $flow, flowFile) {
 		$scope.currentlyUploading=true;
+		// Start the watcher if it has not been started before.
 	    });
+
+	    // (2) Fired when all downloads are complete
 	    $scope.$on('flow::complete', function (event, $flow, flowFile) {
 		$scope.currentlyUploading=false;
 	    });
+
+	    // (3) Fired when one download completes
 	    $scope.$on('flow::fileSuccess', function (file, message, chunk) {
-		$scope.successfullyUploaded=$scope.successfullyUploaded+1;
+		//$scope.successfullyUploaded=$scope.successfullyUploaded+1;
+		getterService.getDatasets().then(function(dataResponse) {
+		    $scope.datasets = dataResponse['data'];
+		    $scope.successfullyUploaded=dataResponse['data'].length;
+		});
 		// Call the API object
 	    });
 
 	}])
+    
 	.directive('tab', function() {
+	    // Simple logic to create a <tab> directive in angular/bootstrap
 	    return {
 		restrict: 'E',
 		transclude: true,
@@ -35,6 +67,7 @@
 	    }
 	})    
 	.directive('tabset', function() {
+	    // A tabset is a group of tabs.
 	    return {
 		restrict: 'E',
 		transclude: true,
