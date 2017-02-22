9b5991c22f94ea43461242cd86d152c159428d99
maxime <maxime@alice>
2017-02-22 15:37:53 +0100

    Preanalysis: added 'parsed' field to the database

--- SPTGUI/models.py
+++ SPTGUI/models.py
@@ -21,13 +21,14 @@ CHOICES_PREA = [('error', 'An unknown error occurred'),
                 ('inprogress', 'Preprocessing in progress'),
                 ('fileformaterror', 'Unrecognized file format'),
                 ('ok', 'Preprocessing completed with success')]
-    
+
 class Dataset(models.Model):
     """Contains datasets and some data about the dataset"""
     analysis = models.ForeignKey(Analysis, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
     data = models.FileField(upload_to='uploads/', default='default.jpg', null=True, blank=True)
+    parsed = models.FileField(upload_to='uploads/', null=True, blank=True)
     unique_id = models.CharField(default='', max_length=200)
     upload_status = models.BooleanField(default=False)
     preanalysis_status = models.CharField(default='na', choices=CHOICES_PREA, max_length=100)
