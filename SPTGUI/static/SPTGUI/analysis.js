400b5b1cccb3673c3a8e5373f454c47c65185e9f
maxime <maxime@alice>
2017-03-27 16:16:26 -0700

    Integrating D3 and Angular :)

--- SPTGUI/static/SPTGUI/analysis.js
+++ SPTGUI/static/SPTGUI/analysis.js
@@ -17,7 +17,7 @@
 	    $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
 	}])
     
-	.service('getterService', ['$http', function($http) {
+	.service('getterService', ['$http', '$rootScope', function($http, $rootScope) {
 	    // This service handles $http requests to get the list for the datasets
 	    this.getDatasets = function(callback) {
 		return $http.get('./api/datasets/');
@@ -43,6 +43,11 @@
 
 	    this.getPreprocessing = function(callback) {
 		return $http.get('./api/preprocessing/');
+	    };
+	    
+	    this.broadcastDataset = function(datasets) {
+		// Broadcast a message that a dataset has been edited
+		$rootScope.$broadcast('datasets:updated',datasets);
 	    };
 	    
 	}])
@@ -88,6 +93,7 @@
 			getterService.getStatistics().then(function(dataResponse) {
 			    $scope.statistics = dataResponse['data'];
 			}); // Populate the scope with the already computed statistics
+			getterService.broadcastDataset($scope.datasets);
 		    });
 		if ($scope.showStatistics == dataset) {
 		    $scope.shownStatistics = null;
@@ -111,6 +117,7 @@
 		});
 		// Finish by sending the updated entry to the server
 		getterService.updateDataset(dataset.id, dataset)
+		    .then(getterService.broadcastDataset($scope.datasets));
 
 		$scope.cancelEdit(dataset); // End edition
 	    }
@@ -191,23 +198,146 @@
     	.controller('ModelingController', ['getterService', '$scope', '$interval', function(getterService, $scope, $interval) {
 	    // This is the controller for the modeling tab
 
-	    // Initiate the window with what we have /!\ SHOULD GO INTO A WATCHER FOR THE TAB SWITCHING STUFF
+	    // Scope variables
+	    $scope.analysisState = 'notrun';
+
+	    // Initiate the window with what we have
 	    getterService.getDatasets().then(function(dataResponse) {
 		$scope.datasets = dataResponse['data'];
 	    }); // Populate the scope with the already uploaded datasets
 	    getterService.getStatistics().then(function(dataResponse) {
 		$scope.statistics = dataResponse['data'];
-	    }); // Populate the scope with the already computed statistics	    
+	    }); // Populate the scope with the already computed statistics
+	    $scope.$on('datasets:updated', function(event,data) {
+		$scope.datasets = data
+	    }); // Look for updated datasets
+	    
 	    //
 	    // ==== CRUD modeling parameters
 	    //
+
+	    // Should also contain the datasets to include
 	    $scope.modelingParameters = {bin_size : 10,
 					 random : 3,
 					};
 	    //$scope.saveParameters = function(modelingParameters) {
 	    //$scope.modelingParameters = scope.modelingParameters
 	    //};
+
+	    //
+	    // ==== Analysis computation logic
+	    //
+
+	    // Function that runs the analysis
+	    $scope.runAnalysis = function(parameters) {
+		// Send the POST command to the server
+		// Show a progress bar (synced from messages from the broker)
+		// Display a graph
+		$scope.jlhist = [{'x': 1,'y': 5}, 
+				 {'x': 20,'y': 20}, 
+				 {'x': 40,'y': 10}, 
+				 {'x': 60,'y': 40}, 
+				 {'x': 80,'y': 5},
+				 {'x': 100,'y': parameters.random}];
+		$scope.$applyAsync();
+		$scope.analysisState='done'; // 'running' for progress bar
+	    }
 	}])
+
+	.directive('jumpLengthHistogram', function() {
+	    function link(scope, el, attr){
+		var lineData = scope.data;
+
+		var vis = d3.select(el[0]).append('svg');
+
+		WIDTH = 300
+		HEIGHT = 300
+		vis.attr({width: WIDTH, height: HEIGHT});
+		
+		MARGINS = {top: 20, right: 20, bottom: 20, left: 50}
+		xRange = d3.scale.linear().range([MARGINS.left,
+						  WIDTH - MARGINS.right]
+						).domain([
+						    d3.min(lineData, function (d) {return d.x;}),
+						    d3.max(lineData, function (d) {return d.x;})]);
+
+		yRange = d3.scale.linear().range([HEIGHT - MARGINS.top,
+						  MARGINS.bottom]
+						).domain([
+						    d3.min(lineData, function (d) {return d.y;}),
+						    d3.max(lineData, function (d) {return d.y;})]);
+		xAxis = d3.svg.axis().scale(xRange).tickSize(5).tickSubdivide(true);
+		yAxis = d3.svg.axis().scale(yRange).tickSize(5).orient("left").tickSubdivide(true);
+
+
+		vis.append("svg:g")
+		    .attr("class", "x axis")
+		    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+		    .call(xAxis);
+
+		vis.append("svg:g")
+		    .attr("class", "y axis")
+		    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+		    .call(yAxis);
+
+		var lineFunc = d3.svg.line()
+		    .x(function (d) {
+			return xRange(d.x);
+		    })
+		    .y(function (d) {
+			return yRange(d.y);
+		    })
+		    .interpolate('basis');
+		
+		vis.append("svg:path")
+		    .attr("d", lineFunc(lineData))
+		    .attr("stroke", "blue")
+		    .attr("stroke-width", 2)
+		    .attr("fill", "none");
+	    
+		scope.$watch('data', function(lineData){
+		    if(!lineData){ return; }
+		    vis.selectAll("path")
+			.attr("d", lineFunc(lineData))
+			.attr("fill", "none");
+		});
+		    
+    // var color = d3.scale.category10();
+    // var width = 200;
+    // var height = 200;
+    // var min = Math.min(width, height);
+    // var svg = d3.select(el[0]).append('svg');
+    // var pie = d3.layout.pie().sort(null);
+    // var arc = d3.svg.arc()
+    //   .outerRadius(min / 2 * 0.9)
+    //   .innerRadius(min / 2 * 0.5);
+
+    // svg.attr({width: width, height: height});
+
+    // // center the donut chart
+    // var g = svg.append('g')
+    //   .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+    
+    // // add the <path>s for each arc slice
+    // var arcs = g.selectAll('path');
+
+    // scope.$watch('data', function(data){
+    //   if(!data){ return; }
+    //   arcs = arcs.data(pie(data));
+    //   arcs.exit().remove();
+    //   arcs.enter().append('path')
+    //     .style('stroke', 'white')
+    //     .attr('fill', function(d, i){ return color(i) });
+    //   // update all the arcs (not just the ones that might have been added)
+    //   arcs.attr('d', arc);
+    // }, true);
+  }
+  return {
+    link: link,
+    restrict: 'E',
+    scope: { data: '=' }
+  };
+	})
 
 	.directive('tab', function() {
 	    // Simple logic to create a <tab> directive in angular/bootstrap
