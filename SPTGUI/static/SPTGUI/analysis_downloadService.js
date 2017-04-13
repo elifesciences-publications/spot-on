e94d657408bced2c1b062e5bbf4426767d297811
maxime <maxime@alice>
2017-04-12 17:57:08 -0700

    Get the list of available downloads at once.

--- SPTGUI/static/SPTGUI/analysis_downloadService.js
+++ SPTGUI/static/SPTGUI/analysis_downloadService.js
@@ -2,10 +2,22 @@ angular.module('app')
     .service('downloadService', ['$http', '$rootScope', function($http, $rootScope){
 	// This service handles $http requests to get the list for the datasets
 	downloads = []
+	
+	getDownloads().then(function(dataResponse) {
+	    downloads = dataResponse.data
+	    alert('downloads initialized')
+	    $rootScope.$broadcast('downloads:updated', downloads)
+	})
+	
+	
+	this.getDownloads = function() {
+	    return $http.get('./api/download/')
+	}
 
 	// Add a download to the list of downloads
 	this.setDownload = function(params) {
 	    downloads.push(params)
+	    // Send a POST with the download and get back the download id
 	    $rootScope.$broadcast('downloads:updated', downloads)
 	};
 
