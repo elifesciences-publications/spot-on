b7e2edc53d76b23921ad000bdd3ac1314fe8c890
maxime <maxime@alice>
2017-06-01 17:15:48 -0700

    Fixed bug on the homepage that prevented from starting the app

--- SPTGUI/static/SPTGUI/homepage.js
+++ SPTGUI/static/SPTGUI/homepage.js
@@ -42,7 +42,7 @@ validateCAPTCHA = function() {
     if (!googleResponse) {
 	$('<p style="color:red !important" class=error-captcha"><span class="glyphicon glyphicon-remove " ></span> Please fill up the captcha.</p>" ').insertAfter("#recaptcha-response");
 	return false;
-    } else {            
+    } else {
 	return true;
     }
 }
