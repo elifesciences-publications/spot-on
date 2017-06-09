elifesciences-publications/spot-on

commit 372131cbd749a05209ae2165bfe54222c5d56906

Implementing bounds
Fixes #125
Fixes #124
Fixes #50

SPTGUI/static/SPTGUI/analysis_ModelingController.js
@@ -228,12 +228,16 @@ angular.module('app')
 	validateJLDparameters = function(pars) {
 	    isOk = true
 	    if (!pars.BinWidth>0) {return false;}
-	    if (!pars.GapsAllowed>=1) {return false;}
+	    if (!pars.GapsAllowed>=0) {return false;}
 	    if (!pars.JumpsToConsider>=3) {return false;}
 	    if (!pars.MaxJump>0) {return false;}
 	    if (!pars.TimeGap>0) {return false;}
 	    if (pars.useAllTraj) {
 		pars.JumpsToConsider = $scope.jldParametersDefault.JumpsToConsider
+	    }
+	    if (pars.MaxJump/pars.BinWidth>2500) {
+		alert("There should not be more than 2500 bins, ie. Max Jump/Bin Width < 2500. Eaither increase the Bin Width or decrease the Max Jump".)
+		return false
 	    }
 	    return isOk
 	}

SPTGUI/templates/SPTGUI/analysis_kinetic.html
@@ -41,11 +41,11 @@
 		      </tr>
 		      <tr>
 			<td>Number of gaps allowed</td>
-			<td><input min="0" ng-model= "jldParameters.GapsAllowed"  type="number"></td>
+			<td><input min="0" max="2" ng-model= "jldParameters.GapsAllowed"  type="number"></td>
 		      </tr>
 		      <tr>
 			<td>Number of timepoints</td>
-			<td><input ng-model= "jldParameters.TimePoints"  type="number"></td>
+			<td><input min="2" max="20" ng-model= "jldParameters.TimePoints"  type="number"></td>
 		      </tr>
 		      <tr>
 			<td>Jumps to consider</td>
@@ -122,15 +122,15 @@
 			<tr ng-if="modelingParameters.fit2states!==null">
 			  <td> D<sub>bound</sub> (µm²/s): </td>
 			  <td>
-			    min: <input ng-model= "modelingParameters.D_bound[0]"  type="number" step="any" min="0">
-			    max: <input ng-model= "modelingParameters.D_bound[1]"  type="number" step="any" min="0">
+			    min: <input ng-model= "modelingParameters.D_bound[0]"  type="number" step="any" min="0" max="2">
+			    max: <input ng-model= "modelingParameters.D_bound[1]"  type="number" step="any" min="0" max="2">
 			  </td>
 			</tr>
 			<tr ng-if="modelingParameters.fit2states==='true'">
 			  <td> D<sub>free</sub> (µm²/s): </td>
 			  <td>
-			    min: <input ng-model= "modelingParameters.D_free[0]"  type="number" step="any" min="0">
-			    max: <input ng-model= "modelingParameters.D_free[1]"  type="number" step="any" min="0">
+			    min: <input ng-model= "modelingParameters.D_free[0]"  type="number" step="any" min="0" max="25">
+			    max: <input ng-model= "modelingParameters.D_free[1]"  type="number" step="any" min="0" max="25">
 			  </td>
 			</tr>
 			<tr ng-if="modelingParameters.fit2states==='true'">
@@ -143,15 +143,15 @@
 			<tr ng-if="modelingParameters.fit2states==='false'">
 			  <td> D<sub>slow</sub> (µm²/s): </td>
 			  <td>
-			    min: <input ng-model= "modelingParameters.D_med[0]"  type="number" step="any" min="0">
-			    max: <input ng-model= "modelingParameters.D_med[1]"  type="number" step="any" min="0">
+			    min: <input ng-model= "modelingParameters.D_med[0]"  type="number" step="any" min="0" max="10">
+			    max: <input ng-model= "modelingParameters.D_med[1]"  type="number" step="any" min="0" max="10">
 			  </td>
 			</tr>
 			<tr ng-if="modelingParameters.fit2states==='false'">
 			  <td> D<sub>fast</sub> (µm²/s): </td>
 			  <td>
-			    min: <input ng-model= "modelingParameters.D_fast[0]"  type="number" step="any" min="0">
-			    max: <input ng-model= "modelingParameters.D_fast[1]"  type="number" step="any" min="0">
+			    min: <input ng-model= "modelingParameters.D_fast[0]"  type="number" step="any" min="0" max="25">
+			    max: <input ng-model= "modelingParameters.D_fast[1]"  type="number" step="any" min="0" max="25">
 			  </td>
 			</tr>			
 			<tr ng-if="modelingParameters.fit2states==='false'">
@@ -184,8 +184,8 @@
 			    <input  ng-model="modelingParameters.LocError" type="number" step="any" min="0">
 			  </td>
 			  <td ng-show="modelingParameters.fitSigma">
-			    min: <input ng-model="modelingParameters.sigma[0]" type="number" step="any" min="0">
-			    max: <input ng-model="modelingParameters.sigma[1]" type="number" step="any" min="0">
+			    min: <input ng-model="modelingParameters.sigma[0]" type="number" step="any" min="0" max="0.1">
+			    max: <input ng-model="modelingParameters.sigma[1]" type="number" step="any" min="0" max="0.1">
 			  </td>
 			</tr>
 			<tr>
@@ -220,7 +220,7 @@
 			</tr>			
 			<tr>
 			  <td> Iterations: </td>
-			  <td><input type="number" ng-model="modelingParameters.iterations" min="0" max="10"></td>
+			  <td><input type="number" ng-model="modelingParameters.iterations" min="0" max="15"></td>
 			</tr>			  
 			<tr>
 			  <td>&nbsp;</td>

commit aef669f7fedb0daaf3cbebe93167e6eb0836a231

Fixing bug introduced in commit 372131cbd749a05209ae2165bfe54222c5d56906
Wrongly terminated string in ModelingController.js

SPTGUI/static/SPTGUI/analysis_ModelingController.js
@@ -236,7 +236,7 @@ angular.module('app')
 		pars.JumpsToConsider = $scope.jldParametersDefault.JumpsToConsider
 	    }
 	    if (pars.MaxJump/pars.BinWidth>2500) {
-		alert("There should not be more than 2500 bins, ie. Max Jump/Bin Width < 2500. Eaither increase the Bin Width or decrease the Max Jump".)
+		alert("There should not be more than 2500 bins, ie. Max Jump/Bin Width < 2500. Eaither increase the Bin Width or decrease the Max Jump.")
 		return false
 	    }
 	    return isOk